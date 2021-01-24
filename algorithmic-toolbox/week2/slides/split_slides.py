23480675d9d0c08d8dceef30dcc09e8689882ebd
Wes Roach <wesr000@gmail.com>
2021-01-24 17:16:35 -0600

    Integrate with Path.
    Point at new file locations.

--- algorithmic-toolbox/week2/slides/split_slides.py
+++ algorithmic-toolbox/week2/slides/split_slides.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from PyPDF2 import PdfFileReader, PdfFileWriter
 
 
@@ -7,8 +9,10 @@ def split_slides(
     pages_to_keep: list = None,
     remove_pages: list = None,
 ):
-    file_base_name = input_slides.replace(".pdf", "")
-    pdf = PdfFileReader(input_slides)
+    input_slides = Path(input_slides)
+    output_directory = Path(output_directory)
+
+    pdf = PdfFileReader(str(input_slides))
     pdfWriter = PdfFileWriter()
 
     if pages_to_keep is None and remove_pages is None:
@@ -26,7 +30,7 @@ def split_slides(
             page for page in range(1, pdf.getNumPages() + 1) if page not in remove_pages
         ]
 
-    print(f"File: {input_slides}; {len(pages)} Pages: {pages}")
+    print(f"File: {input_slides.name}; {len(pages)} Pages: {pages}")
 
     # PdfFileReader reads/writes 0-indexed
     pages = [page - 1 for page in pages]
@@ -34,7 +38,7 @@ def split_slides(
     for page_num in pages:
         pdfWriter.addPage(pdf.getPage(page_num))
 
-    with open(f"{output_directory}/{file_base_name}.pdf", "wb") as f:
+    with open(output_directory / input_slides.name, "wb") as f:
         pdfWriter.write(f)
         f.close()
 
@@ -42,8 +46,8 @@ def split_slides(
 # 1-intro-2-fibonaccinumbers.pdf
 # These pages numbers are the displayed page numbers
 split_slides(
-    "1-intro-2-fibonaccinumbers.pdf",
-    "reduced-slides",
+    "original/1-intro-2-fibonaccinumbers.pdf",
+    "reduced",
     remove_pages=[
         4,
         *range(7, 10),
@@ -61,40 +65,40 @@ split_slides(
 
 # 1-intro-3-GCD1.pdf
 # These pages numbers are the displayed page numbers
-split_slides("1-intro-3-GCD1.pdf", "reduced-slides", remove_pages=[3, 4, 7, 8, 10])
+split_slides("original/1-intro-3-GCD1.pdf", "reduced", remove_pages=[3, 4, 7, 8, 10])
 
 # 1-intro-3-GCD2.pdf
 # These pages numbers are the displayed page numbers
 split_slides(
-    "1-intro-3-GCD2.pdf", "reduced-slides", remove_pages=[3, 7, *range(9, 15), 16],
+    "original/1-intro-3-GCD2.pdf", "reduced", remove_pages=[3, 7, *range(9, 15), 16],
 )
 
 # 1-intro-4-1-runtimes.pdf
 # These pages numbers are the displayed page numbers
-split_slides("1-intro-4-1-runtimes.pdf", "reduced-slides", remove_pages=[4, 18])
+split_slides("original/1-intro-4-1-runtimes.pdf", "reduced", remove_pages=[4, 18])
 
 # 1-intro-4-2-asymptoticnotation1.pdf
 # These pages numbers are the displayed page numbers
 split_slides(
-    "1-intro-4-2-asymptoticnotation1.pdf", "reduced-slides", remove_pages=[4, 9, 10]
+    "original/1-intro-4-2-asymptoticnotation1.pdf", "reduced", remove_pages=[4, 9, 10]
 )
 
 # 1-intro-4-2-asymptoticnotation2.pdf
 # These pages numbers are the displayed page numbers
 split_slides(
-    "1-intro-4-2-asymptoticnotation2.pdf", "reduced-slides", remove_pages=[3, 9, 10]
+    "original/1-intro-4-2-asymptoticnotation2.pdf", "reduced", remove_pages=[3, 9, 10]
 )
 
 # 1-intro-4-2-asymptoticnotation3.pdf
 # These pages numbers are the displayed page numbers
 split_slides(
-    "1-intro-4-2-asymptoticnotation3.pdf",
-    "reduced-slides",
+    "original/1-intro-4-2-asymptoticnotation3.pdf",
+    "reduced",
     remove_pages=[*range(4, 8), *range(10, 17)],
 )
 
 # 1-intro-5-courseoverview.pdf
 # These pages numbers are the displayed page numbers
 split_slides(
-    "1-intro-5-courseoverview.pdf", "reduced-slides", remove_pages=[6, *range(8, 11)],
+    "original/1-intro-5-courseoverview.pdf", "reduced", remove_pages=[6, *range(8, 11)],
 )
