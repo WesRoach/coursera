a370cee7664f62dea62d1e24c6c7965d05f57709
Wes Roach <wesr000@gmail.com>
2021-01-24 22:13:22 -0600

    Complete exercise 3.

---
+++ algorithmic-toolbox/week2/solutions/3_greatest_common_divisor/cpp/ReadMe.md
@@ -0,0 +1,5 @@
+# C++
+
+```c++
+g++ -pipe -O2 -std=c++14 gcd.cpp -lm
+```
---
+++ algorithmic-toolbox/week2/solutions/3_greatest_common_divisor/cpp/gcd.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <cassert>
+
+int gcd_naive(int a, int b)
+{
+    int current_gcd = 1;
+    for (int d = 2; d <= a && d <= b; d++)
+    {
+        if (a % d == 0 && b % d == 0)
+        {
+            if (d > current_gcd)
+            {
+                current_gcd = d;
+            }
+        }
+    }
+    return current_gcd;
+}
+
+int gcd_fast(int a, int b)
+{
+    int tmp_swap = 0;
+    while (b > 0)
+    {
+        // a, b = b, a % b
+        tmp_swap = b;
+        b = a % b;
+        a = tmp_swap;
+    }
+    return a;
+}
+
+void test_solution()
+{
+    assert(gcd_fast(18, 35) == 1);
+    assert(gcd_fast(28851538, 1183019) == 17657);
+}
+
+int main()
+{
+    // test_solution();
+
+    int a, b;
+    std::cin >> a >> b;
+    std::cout << gcd_naive(a, b) << std::endl;
+
+    return 0;
+}
