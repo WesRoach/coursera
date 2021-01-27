f5a9e67398ad4ae29873217b3c173a07f3948160
Wes Roach <wesr000@gmail.com>
2021-01-26 22:25:18 -0600

    Turns out my test case was wrong.
    Wasted a lot of time.
    Awesome.

--- algorithmic-toolbox/week2/solutions/7_last_digit_of_the_sum_of_fibonacci_numbers_again/python/fibonacci_partial_sum.py
+++ algorithmic-toolbox/week2/solutions/7_last_digit_of_the_sum_of_fibonacci_numbers_again/python/fibonacci_partial_sum.py
@@ -1,10 +1,27 @@
+def fib_pisano_period(m):
+    pisano_period = [0, 1]
+    prev, current = 0, 1
+    while True:
+        current, prev = (current + prev) % m, current
+        if current == 1 and prev == 0:
+            return pisano_period[:-1]
+        pisano_period.append(current)
+
+
+def fibonacci_sum(n):
+    pisano_period = fib_pisano_period(10)
+    pisano_period_sum = sum(pisano_period)
+    return ((pisano_period_sum * (n // 60)) + sum(pisano_period[: (n % 60) + 1])) % 10
+
+
 def fibonacci_partial_sum(start: int, end: int):
-    prev, current, _sum = 0, 1, 0
-    for i in range(1, end + 1):
-        if i >= start:
-            _sum += current
-        current, prev = (current + prev) % 10, current
-    return _sum % 10
+    pisano_period = fib_pisano_period(10)
+    if start == end:
+        return pisano_period[start % 60]
+
+    start_fib = fibonacci_sum(start - 1)
+    end_fib = fibonacci_sum(end)
+    return (end_fib - start_fib) % 10
 
 
 if __name__ == "__main__":
