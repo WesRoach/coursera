f87a6ef5bea831783c9486ce99e980d78e63ec05
Wes Roach <wesr000@gmail.com>
2021-03-26 14:32:28 -0500

    Complete week5 p1.

--- algorithmic-toolbox/week5/solutions/1_money_change_again/python/change_dp.py
+++ algorithmic-toolbox/week5/solutions/1_money_change_again/python/change_dp.py
@@ -1,14 +1,27 @@
-def get_change(m):
+import math
+import pprint
+
+
+def get_change(m, coins: list = [1, 3, 4]):
     """minimum number coins with denominators 1, 3, 4 which changes m
 
     Args:
         m (int): money
+        coins (List[int]): coin denominations
 
     Returns:
         int: minimum number of coins to reach m
     """
-    # write your code here
-    return m // 4
+    min_num_coins = {0: 0}
+    for m in range(1, m + 1):
+        min_num_coins[m] = math.inf
+        for coin in coins:
+            if m >= coin:
+                num_coins = min_num_coins[m - coin] + 1
+                if num_coins < min_num_coins[m]:
+                    min_num_coins[m] = num_coins
+
+    return min_num_coins[m]
 
 
 if __name__ == "__main__":
