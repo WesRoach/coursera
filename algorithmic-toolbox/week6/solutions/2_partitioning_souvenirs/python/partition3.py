39bdcf69578893b933858ff4210948b30176d0b7
Wes Roach <wesr000@gmail.com>
2021-04-19 23:30:24 -0500

    Just setting up week6 exercises.

--- algorithmic-toolbox/week6/solutions/2_partitioning_souvenirs/python/partition3.py
+++ algorithmic-toolbox/week6/solutions/2_partitioning_souvenirs/python/partition3.py
@@ -1,8 +1,16 @@
-# Uses python3
-import sys
 import itertools
+from typing import List
 
-def partition3(A):
+
+def partition3(A: List[int]) -> int:
+    """Determine if possible to partition A into three equal sums.
+
+    Args:
+        A (List[int]): list of ints
+
+    Returns:
+        int: 1: True; 0: False
+    """
     for c in itertools.product(range(3), repeat=len(A)):
         sums = [None] * 3
         for i in range(3):
@@ -13,8 +21,28 @@ def partition3(A):
 
     return 0
 
-if __name__ == '__main__':
-    input = sys.stdin.read()
-    n, *A = list(map(int, input.split()))
-    print(partition3(A))
 
+def partition3_brute(A: List[int]) -> int:
+    """Determine if possible to partition A into three equal sums.
+
+    Args:
+        A (List[int]): list of ints
+
+    Returns:
+        int: 1: True; 0: False
+    """
+    for c in itertools.product(range(3), repeat=len(A)):
+        sums = [None] * 3
+        for i in range(3):
+            sums[i] = sum(A[k] for k in range(len(A)) if c[k] == i)
+
+        if sums[0] == sums[1] and sums[1] == sums[2]:
+            return 1
+
+    return 0
+
+
+if __name__ == "__main__":
+    n = input()
+    A = list(map(int, input().split()))
+    print(partition3(A))
